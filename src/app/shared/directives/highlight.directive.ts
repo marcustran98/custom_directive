0d0cd40a0479812f8e484b744fca6f29b3cb25d5
dung <dungkycuc@gmail.com>
2021-07-23 17:06:32 +0700

    done tooltip

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -6,18 +6,13 @@ import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 export class HightLightDirective {
   @HostListener('mouseover') mouseOOver() {
     (<HTMLElement>this.el.nativeElement).style.backgroundColor = 'red';
-
-    const div = this.renderer.createElement('div');
-    const text = this.renderer.createText('Hello world!');
-    this.renderer.appendChild(div, text);
-    this.renderer.appendChild(this.el.nativeElement, div);
   }
 
   @HostListener('mouseleave') mouseOMove() {
     (<HTMLElement>this.el.nativeElement).style.backgroundColor = 'yellow';
   }
 
-  @HostListener('click') onCLick() {}
+  @HostListener('click') onCLick() { }
   constructor(private el: ElementRef, private renderer: Renderer2) {
     console.log('ele', el);
     (<HTMLElement>this.el.nativeElement).style.backgroundColor = 'yellow';
--- src/app/shared/directives/tooltip.directive.ts
+++ src/app/shared/directives/tooltip.directive.ts
@@ -6,15 +6,44 @@ import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 })
 export class TooltipDirective implements OnChanges {
   @Input() tooltip!: string;
+  @Input() delay!: string;
+  customTitle!: HTMLElement;
+  count = 0;
   @HostListener('mouseover') onMouseOver() {
-    (this.el.nativeElement as HTMLElement).style.backgroundColor = 'pink';
-    const span = this.renderer.createElement('span');
-    const text = this.renderer.createText(this.tooltip);
-    this.renderer.appendChild(span, text);
-    (span as HTMLElement).classList.add('tooltip-ne');
-    this.renderer.appendChild(this.el.nativeElement, span);
+    console.log("this.customTitle", this.customTitle);
+    if (!this.count) {
+      this.create();
+    }
   }
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  @HostListener("mouseleave") onLeave() {
+    this.remove();
+  }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  create(): void {
+    (this.el.nativeElement as HTMLElement).style.backgroundColor = 'pink';
+    this.customTitle = this.renderer.createElement('span');
+    const text = this.renderer.createText(this.tooltip);
+    this.renderer.appendChild(this.customTitle, text);
+    this.customTitle.style.transition = `${+this.delay ? +this.delay / 1000 : 0}s`;
+    this.customTitle.classList.add('tooltip-ne');
+    setTimeout(() => {
+      this.customTitle.style.opacity = "1";
+    }, +this.delay)
+    this.renderer.appendChild(this.el.nativeElement, this.customTitle);
+    this.count = 1;
+  }
+
+  remove(): void {
+    if (this.count) {
+      this.customTitle.style.opacity = "0";
+      setTimeout(() => {
+        this.renderer.removeChild(this.el.nativeElement, this.customTitle);
+        this.count = 0;
+      }, +this.delay || 0)
+    }
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     console.log(changes);
   }
